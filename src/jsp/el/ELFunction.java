cead565dd991b3f9204462c700d3a5157cd38bce
mybirth0407 <mybirth0407@gmail.com>
2016-04-04 01:15:18 +0900

    el 삭제 후 복구

---
+++ src/jsp/el/ELFunction.java
@@ -0,0 +1,27 @@
+package jsp.el;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ELFunction {
+    public static int length(Object obj) {
+        if (obj == null) {
+            return 0;
+        }
+
+        if (obj instanceof String) {
+            return ((String)obj).length();
+        }
+
+        if (obj instanceof Collection) {
+            return ((Collection)obj).size();
+        }
+
+        if (obj instanceof Map) {
+            return ((Map)obj).size();
+        }
+        else {
+            return 0;
+        }
+    }
+}
